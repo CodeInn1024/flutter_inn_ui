5c9e4cf5a539a1d8edcba97ee38e1380ee3a611c
CodeInn1024 <57973749+CodeInn1024@users.noreply.github.com>
2019-12-13 17:56:04 +0800

    时间转换添加字符窜

--- lib/untils/format_date.dart
+++ lib/untils/format_date.dart
@@ -4,11 +4,12 @@
  * @Autor: lqrui.cn
  * @Date: 2019-12-05 11:11:07
  * @LastEditors: lqrui.cn
- * @LastEditTime: 2019-12-09 11:33:34
+ * @LastEditTime: 2019-12-13 17:45:57
 */
 
 import 'package:flutter_lqrui/lqr_common.dart';
 
 class LqrDateTime {
-  static formatDateMs(int val, {String format}) => DateUtil.formatDateMs(val, format: format ?? "yyyy-M-d HH:mm:ss");
+  static String formatDateMs(int val, {String format}) => DateUtil.formatDateMs(val, format: format ?? "yyyy-M-d HH:mm:ss");
+  static String formatDateMsStr(String val, {String format}) => val == "" ? "" : DateUtil.formatDateMs(int.parse(val), format: format ?? "yyyy-M-d HH:mm:ss");
 }
